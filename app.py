c79c9cf175c34731cc6682c0023f5c83a4870af5
arvis <viestards.lists@gmail.com>
2013-10-09 23:48:49 +0300

    better looking, form list, posting some data to server

--- app.py
+++ app.py
@@ -1,22 +1,46 @@
-from flask import Flask
+from flask import Flask,jsonify,json
 from flask import render_template
+from flask import request
 
 app = Flask(__name__)
+
+form_data=[{'name':'form1',"title":"form 1"}]
+
+
 
 @app.route("/")
 def main():
     return render_template('index.html')
-    #return "Hello World!"
 
 @app.route("/admin")
 def admin():
+    return render_template('admin_menu.html')
+    
+@app.route("/admin/forms")
+def admin_form():
     return render_template('admin.html')
-    #return "Hello World!"
+    
+
 
 @app.route("/show/<form_name>")
 def show_form(form_name):
     return "This is form <input id='my_form' /> "+form_name 
 
+
+
+@app.route("/admin/form_list")
+def show_forms_json():
+    #TODO: replace with dynamic not dymmy data
+    #form_data.append({'name':'form6',"title":"form 6"})
+    return json.dumps(form_data)
+
+@app.route("/admin/add_form",methods=['POST'])
+def add_form_json():
+    #return "aaaa"
+    print request.json
+    #print request.form['form_name']
+    form_data.append({'name':'form6',"title":"form 6"})
+    return "eee"
 
 
 
--- static/admin.js
+++ static/admin.js
@@ -1,3 +1,12 @@
+function FormListCtrl($scope, $http) {
+  $http.get('/admin/form_list').success(function(data) {
+    $scope.forms_list = data;
+  });
+ 
+}
+
+
+
 function PhoneListCtrl($scope, $http) {
   $http.get('static/form_data.json').success(function(data) {
     $scope.phones = data;
@@ -6,26 +15,40 @@ function PhoneListCtrl($scope, $http) {
   $scope.orderProp = 'age';
 }
  
-//PhoneListCtrl.$inject = ['$scope', '$http'];
 
 
-function DbFieldController($scope) {
+function DbFieldController($scope,$http) {
     $scope.master= [];
+    //$scope.formProps.form_type={id:"single_record"};
+    
 
     $scope.add = function(dbField) {
         //$scope.master= angular.copy(dbField);
         //angular.extend($scope.master, dbField);
         $scope.master.push(dbField);
         $scope.dbField ={};
-       
+        $scope.showAddField=false;
     };
+    
+    $scope.set_data = function(field_name) {
+        console.log(field_name);
+        
+        $scope.dbField =field_name;
+    };
+    
+    $scope.post_form_data= function(){
+        $http.post('/admin/add_form', $scope.master).success(function(data) {
+            //TODO: show some notification
+            console.log("data saved");
+        });     
+    };
+    
+    
+    
 
     $scope.reset = function() {
         $scope.user = angular.copy($scope.master);
     };
-
-    //  $scope.dbField.field_type = 'text_field';
-
 
     $scope.reset();
 }
