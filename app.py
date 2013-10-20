3d989d4c3ef2e7e447edb433e2a7069462fbcabb
arvis <viestards.lists@gmail.com>
2013-10-20 21:31:24 +0300

    adding table edit and mongodb

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask,jsonify,json
 from flask import render_template
 from flask import request
+from flask import Flask
+from flask.ext.pymongo import PyMongo
+from bson import json_util
 
 app = Flask(__name__)
+mongo = PyMongo(app)
 
-form_data=[{'name':'form1',"title":"form 1"}]
+#form_data=[{'name':'form1',"title":"form 1",'fields':[{ }] }]
 
+forms_list=[{'name':'form1',"title":"form 1","data":[{"name":"field1","type":"text"},{"name":"field2","type":"text"}] }]
 
 
 @app.route("/")
@@ -21,11 +26,74 @@ def admin_form():
     return render_template('admin.html')
     
 
+def render_form_header(form_data):
+    form_head= "<h3> %s </h3>" % (form_data["title"])
+    form_head+='<form ng-submit="set_field()">'
+    return form_head
 
-@app.route("/show/<form_name>")
+def render_form_footer():
+    return '<input type="submit" class="btn btn-default" value="Submit">'
+
+
+def render_input(data):
+    input_data='<input type="text" class="form-control" id="%s" placeholder="%s" ng-model="formProps.%s" required />' % (data["name"],data["name"],data["name"], )
+    return input_data
+    
+
+def render_form(form_id):
+    form_data=forms_list[0]
+    form_output=render_form_header(form_data)
+    for field in form_data["data"]:
+        form_output+=render_input(field)
+    form_output+=render_form_footer()
+    return form_output
+
+
+def render_list_definition(form_name):
+    form_data=forms_list[0]
+    output='<table class="table"><tr>'
+    for field in form_data["data"]:
+        output+='<th> %s </td>' % (field["name"] )
+    
+    output+='</tr><tr ng-repeat="field in data ">'
+    for field in form_data["data"]:
+        output+='<td ng-bind="field.%s"> </td>' % (field["name"] )
+    output+="</tr></table>"
+    return output 
+    
+def render_list_head(form_name):
+    pass
+
+@app.route("/show_list/<form_name>",methods=['GET'])
+def show_list(form_name):
+    out=render_list_definition(form_name)
+    print out
+    return out
+
+@app.route("/show_form/<form_name>",methods=['GET'])
 def show_form(form_name):
-    return "This is form <input id='my_form' /> "+form_name 
+    form_data=render_form(form_name)
+    return form_data
 
+
+
+@app.route("/post_data/",methods=['POST'])
+def post_form():
+    data=request.json["data"]
+    form_name=request.json["form_name"]
+    print data,form_name
+    mongo.db.test_data.save(data)
+    return "success"
+
+@app.route("/data/<form_name>",methods=['GET'])
+def get_all(form_name):
+    docs = mongo.db["test_data"].find()
+    
+    #import pdb; pdb.set_trace()
+    
+    json_docs=json.dumps(list(docs), default=json_util.default)
+    #return render_template('all.html',docs=docs)
+    return json_docs 
 
 
 @app.route("/admin/form_list")
--- static/ang.js
+++ static/ang.js
@@ -1,9 +1,38 @@
-function PhoneListCtrl($scope, $http) {
-  $http.get('static/form_data.json').success(function(data) {
-    $scope.phones = data;
-  });
  
-  $scope.orderProp = 'age';
+function SingleFormController($scope, $http) {
+  //$scope.data=[];  
+  $scope.form_name="sample_form_name";  
+  $scope.formProps={};
+  
+  $http.get('data/aaa').success(function(data) {
+    $scope.data = data;
+  });
+
+  
+  
+  $scope.set_data=function(){
+    //$http.post('/someUrl', data).success(successCallback);
+    var data={};
+    data.userid=1;
+    data.form_name=$scope.form_name;
+    data.data=$scope.formProps;
+    console.log(data);  
+    
+    $http.post('/post_data/', data).
+      success(function(data, status, headers, config) {
+          $scope.formProps={};
+          console.log("success");
+          
+      }).
+      error(function(data, status, headers, config) {
+          console.log("failure");
+      });
+    
+    
+    
+      
+  };
+    
 }
  
 //PhoneListCtrl.$inject = ['$scope', '$http'];
@@ -22,3 +51,58 @@ function Controller($scope) {
  
   $scope.reset();
 }
+
+/*
+
+var phonecatServices = angular.module('phonecatServices', ['ngResource']);
+
+phonecatServices.factory('Phone', ['$resource',
+  function($resource){
+    return $resource('phones/:phoneId.json', {}, {
+      query: {method:'GET', params:{phoneId:'phones'}, isArray:true}
+    });
+  }]);
+
+var phonecatApp = angular.module('phonecatApp', [
+  'phonecatControllers',
+  'phonecatFilters',
+  'phonecatServices'
+]);
+
+phonecatApp.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/phones', {
+        templateUrl: 'partials/phone-list.html',
+        controller: 'PhoneListCtrl'
+      }).
+      when('/phones/:phoneId', {
+        templateUrl: 'partials/phone-detail.html',
+        controller: 'PhoneDetailCtrl'
+      }).
+      otherwise({
+        redirectTo: '/phones'
+      });
+  }]);
+  
+  
+var phonecatControllers = angular.module('phonecatControllers', []);
+
+phonecatControllers.controller('PhoneListCtrl', ['$scope', 'Phone',
+  function($scope, Phone) {
+    $scope.phones = Phone.query();
+    $scope.orderProp = 'age';
+  }]);
+
+phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', 'Phone',
+  function($scope, $routeParams, Phone) {
+    $scope.phone = Phone.get({phoneId: $routeParams.phoneId}, function(phone) {
+      $scope.mainImageUrl = phone.images[0];
+    });
+
+    $scope.setImage = function(imageUrl) {
+      $scope.mainImageUrl = imageUrl;
+    }
+  }]);
+
+*/
